3ceb75ace94dfc43e59881d3c9c49b504e6f04ab
Jackie Feminella <jaclynfeminella@gmail.com>
2019-06-20 17:07:03 -0400

    Change deploy command

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,13 @@
+module.exports = [{
+      plugin: require('/Users/jaclynf.eminella/Workspace/imperfectproduce.github.io/node_modules/gatsby-plugin-google-analytics/gatsby-browser.js'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('/Users/jaclynf.eminella/Workspace/imperfectproduce.github.io/node_modules/gatsby-plugin-offline/gatsby-browser.js'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('/Users/jaclynf.eminella/Workspace/imperfectproduce.github.io/node_modules/gatsby-plugin-typography/gatsby-browser.js'),
+      options: {"plugins":[],"pathToConfigModule":"src/utils/typography"},
+    },{
+      plugin: require('/Users/jaclynf.eminella/Workspace/imperfectproduce.github.io/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
