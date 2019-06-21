cf697815943ea02f1ff7b7109f23e97b4197cc4d
Jackie Feminella <jaclynfeminella@gmail.com>
2019-06-21 09:11:49 -0400

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-d4a3a456037ecafca5d8.js"
+    "url": "webpack-runtime-1d0ca198afa5f94ae174.js"
   },
   {
     "url": "app.c24795aec2fa30a019ec.css"
@@ -39,7 +39,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "49db32f1efd9f3e140c4973fe392d115"
+    "revision": "3c6710c08db82cfbeaad977f568671a2"
   },
   {
     "url": "component---src-pages-404-js-238acc640550e13d6c90.js"
